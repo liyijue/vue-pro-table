8928688d971173c043f9dc07f2b385817fd0edb3
liyijue <1754812312@qq.com>
2023-03-21 12:28:50 +0800

    feat(ProForm): 给表单添加双向绑定

--- src/components/ProForm/ProForm.vue
+++ src/components/ProForm/ProForm.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { ArrowDownBold, ArrowUp } from "@element-plus/icons-vue";
 import { ref, reactive } from "vue";
+import type { FormInstance } from "element-plus";
 import { calculateExpandSpanMaxIndex } from "../../utils/utils";
 import "element-plus/theme-chalk/display.css";
 
@@ -9,14 +10,27 @@ const props = withDefaults(defineProps<{ columns: any; span: any }>(), {
   span: { xs: 8, sm: 6, md: 6, lg: 4, xl: 4 },
 });
 
+const ruleFormRef = ref<FormInstance>();
 const isExpand = ref(false);
 const hiddenLayoutIndex = reactive(calculateExpandSpanMaxIndex(props.span));
-console.log(hiddenLayoutIndex)
+const form = reactive<any>({});
+
+const handleReset = () => {
+  ruleFormRef.value?.resetFields();
+};
+const handleSubmit = () => {
+  console.log(form);
+};
 </script>
 
 <template>
   <el-card shadow="never" v-if="columns.length">
-    <el-form class="pro_form" label-width="auto">
+    <el-form
+      :model="form"
+      ref="ruleFormRef"
+      class="pro_form"
+      label-width="auto"
+    >
       <el-row :gutter="40">
         <el-col
           v-bind="span"
@@ -30,8 +44,13 @@ console.log(hiddenLayoutIndex)
             'hidden-xs-and-down': index > hiddenLayoutIndex.xs && !isExpand,
           }"
         >
-          <el-form-item :label="col.title" :name="col.dataIndex">
+          <el-form-item
+            :label="col.title"
+            :name="col.dataIndex"
+            :prop="col.dataIndex"
+          >
             <component
+              v-model="form[col.dataIndex]"
               :is="col.type || 'el-input'"
               placeholder="请输入"
               v-bind="col.filedProps"
@@ -41,8 +60,8 @@ console.log(hiddenLayoutIndex)
         <el-col :xs="8" :sm="6" :md="6" :lg="4" :offset="isExpand ? 20 : 0">
           <el-form-item>
             <el-space>
-              <el-button>重置</el-button>
-              <el-button type="primary">查询</el-button>
+              <el-button @click="handleReset">重置</el-button>
+              <el-button type="primary" @click="handleSubmit">查询</el-button>
               <div class="pro_form_expand" @click="isExpand = !isExpand">
                 <template v-if="!isExpand">
                   <el-space>
